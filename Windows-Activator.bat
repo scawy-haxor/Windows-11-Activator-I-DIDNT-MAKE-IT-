4adc1c0408c781bf352337297410d9622418c177
!!!SC4WY H4XOR!!! <119148542+scawy-haxor@users.noreply.github.com>
2026-09-18 10:46:38 +0700

    Update Windows-Activator.bat

--- Windows-Activator.bat
+++ Windows-Activator.bat
@@ -1,22 +1,81 @@
-@echo off
-title Activate Windows 11 (ALL versions) for FREE - MSGuides.com&cls&echo =====================================================================================&echo #Project: Activating Microsoft software products for FREE without additional software&echo =====================================================================================&echo.&echo #Supported products:&echo - Windows 11 Home&echo - Windows 11 Professional&echo - Windows 11 Education&echo - Windows 11 Enterprise&echo.&echo.&echo ============================================================================&echo Activating your Windows...&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo slmgr.vbs /upk >nul&cscript //nologo slmgr.vbs /cpky >nul&set i=1&wmic os | findstr /I "enterprise" >nul
-if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43 >nul||cscript //nologo slmgr.vbs /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 >nul||cscript //nologo slmgr.vbs /ipk YYVX9-NTFWV-6MDM3-9PT4T-4M68B >nul||cscript //nologo slmgr.vbs /ipk 44RPN-FTY23-9VTTB-MP9BX-T84FV >nul||cscript //nologo slmgr.vbs /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9 >nul||cscript //nologo slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ >nul||cscript //nologo slmgr.vbs /ipk DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ >nul||cscript //nologo slmgr.vbs /ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639 >nul||cscript //nologo slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D >nul||cscript //nologo slmgr.vbs /ipk 92NFX-8DJQP-P6BBQ-THF9C-7CG2H >nul&goto skms) else wmic os | findstr /I "home" >nul
-if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 >nul||cscript //nologo slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM >nul||cscript //nologo slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH >nul||cscript //nologo slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR >nul&goto skms) else wmic os | findstr /I "education" >nul
-if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 >nul||cscript //nologo slmgr.vbs /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ >nul&goto skms) else wmic os | findstr /I "11 pro" >nul
-if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul||cscript //nologo slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9 >nul||cscript //nologo slmgr.vbs /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J >nul||cscript //nologo slmgr.vbs /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF >nul||cscript //nologo slmgr.vbs /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y >nul||cscript //nologo slmgr.vbs /ipk YVWGF-BXNMC-HTQYQ-CPQ99-66QFC >nul&goto skms) else (goto notsupported)
-:skms
-if %i% GTR 10 goto busy
-if %i% EQU 1 set KMS=kms7.MSGuides.com
-if %i% EQU 2 set KMS=kms8.MSGuides.com
-if %i% EQU 3 set KMS=kms9.MSGuides.com
-if %i% GTR 3 goto ato
-cscript //nologo slmgr.vbs /skms %KMS%:1688 >nul
-:ato
-echo ============================================================================&echo.&echo.&cscript //nologo slmgr.vbs /ato | find /i "successfully" && (echo.&echo ============================================================================&echo.&echo #My official blog: MSGuides.com&echo.&echo #How it works: bit.ly/kms-server&echo.&echo #Please feel free to contact me at msguides.com@gmail.com if you have any questions or concerns.&echo.&echo #Please consider supporting this project: donate.msguides.com&echo #Your support is helping me keep my servers running 24/7!&echo.&echo ============================================================================&choice /n /c YN /m "Would you like to visit my blog [Y,N]?" & if errorlevel 2 exit) || (echo The connection to my KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto skms)
-explorer "http://MSGuides.com"&goto halt
-:notsupported
-echo ============================================================================&echo.&echo Sorry, your version is not supported.&echo.&goto halt
-:busy
-echo ============================================================================&echo.&echo Sorry, the server is busy and can't respond to your request. Please try again.&echo.
-:halt
-pause >nul
+@ECHO off
+cls
+cd "C:\Windows\System32"
+
+
+:start
+ECHO WARNING: USE AT YOUR OWN RISK
+ECHO I AM NOT RESPONSIBLE FOR ANY DAMAGES
+set /p c=Would you like to Continue [Y/N]?
+CLS
+if /I "%c%" NEQ "Y" exit
+
+ECHO.
+ECHO 1. Home	
+ECHO 2. Home N	
+ECHO 3. Home Home Single Language	
+ECHO 4. Home Country Specific	
+ECHO 5. Pro	
+ECHO 6. Pro N	
+ECHO 7. Pro for Workstations	
+ECHO 8. Pro for Workstations N	
+ECHO 9. Pro Education	
+ECHO 10. Pro Education N	
+ECHO 11. Education	
+ECHO 12. Education N	
+ECHO 13. Enterprise
+ECHO 14. Enterprise N	
+ECHO 15. Enterprise G	
+ECHO 16. Enterprise G N	
+ECHO 17. Enterprise LTSC 2019	
+ECHO 18. Enterprise N LTSC 2019      
+ECHO 19. End
+
+set choice=
+set /p choice=Select Your Operating System to Continue.
+
+:loop
+
+set "productKey="
+
+if %choice%==1 set productKey=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+if %choice%==2 set productKey=3KHY7-WNT83-DGQKR-F7HPR-844BM 
+if %choice%==3 set productKey=7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+if %choice%==4 set productKey=PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
+if %choice%==5 set productKey=W269N-WFGWX-YVC9B-4J6C9-T83GX
+if %choice%==6 set productKey=MH37W-N47XK-V7XM9-C7227-GCQG9
+if %choice%==7 set productKey=NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+if %choice%==8 set productKey=9FNHH-K3HBT-3W4TD-6383H-6XYWF
+if %choice%==9 set productKey=6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+if %choice%==10 set productKey=YVWGF-BXNMC-HTQYQ-CPQ99-66QFC
+if %choice%==11 set productKey=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+if %choice%==12 set productKey=2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+if %choice%==13 set productKey=NPPR9-FWDCX-D2C8J-H872K-2YT43
+if %choice%==14 set productKey=DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+if %choice%==15 set productKey=YYVX9-NTFWV-6MDM3-9PT4T-4M68B
+if %choice%==16 set productKey=44RPN-FTY23-9VTTB-MP9BX-T84FV
+if %choice%==17 set productKey=M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+if %choice%==18 set productKey=92NFX-8DJQP-P6BBQ-THF9C-7CG2H
+if %choice%==19 goto end
+
+if "%productKey%"=="" (
+    echo "%choice%" is not valid, try again
+    goto start
+)
+
+rem Now that the productKey variable has been set, 
+rem we can proceed to the next step of the process
+rem Activating Windows
+
+cscript slmgr.vbs /ipk %productKey%
+cscript slmgr.vbs /skms kms8.msguides.com
+cscript slmgr.vbs /ato
+
+:end
+ECHO THANKS FOR USING A 
+ECHO Boss_Man PRODUCT
+ECHO.
+set /P c=Would You Like to quit[Y/N]?
+CLS
+if /I "%c%" EQU "Y" goto :Exit
+if /I "%c%" EQU "N" goto :start
